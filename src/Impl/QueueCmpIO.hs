18bfaa90c29bab9b815b2ca4fbbe7b3a6bc736b7
Andre Van Der Merwe <andre@andrevdm.com>
2021-12-18 17:44:39 +0200

    Polling loop and trigger from psql NOTIFY

--- src/Impl/QueueCmpIO.hs
+++ src/Impl/QueueCmpIO.hs
@@ -16,6 +16,7 @@ import qualified UnliftIO.Concurrent as UC
 import qualified Components.LogCmp as CL
 import qualified Components.PsqlCmp as CPg
 import qualified Components.QueueCmp as CQ
+import qualified Threading as Th
 
 
 newQueueCmpIO
@@ -51,8 +52,53 @@ startQueue
   -> CPg.ChanName
   -> m ()
 startQueue pgCmp lgCmp chanName = do
-  CPg.pgListenForNotifications pgCmp chanName (CL.logDebug' lgCmp "LISTEN> ")
+  pollGate <- Th.newOpenGate
 
-  void . UA.async . forever $ do
-    UC.threadDelay 1000
+  CPg.pgListenForNotifications pgCmp chanName $ \n -> do
+    CL.logDebug' lgCmp "LISTEN> " n
+    Th.openGate pollGate
 
+  void . UA.async $ runPoll pollGate tryGetActiveItem
+  void . UA.async $ runTriggerPoll 10 pollGate --TODO get poll period
+
+
+-- | See if there is actually an item to work with
+tryGetActiveItem
+  :: forall m.
+     (MonadUnliftIO m)
+  => m Bool
+tryGetActiveItem = do
+  putText "tryGetActive"
+  pure False
+
+
+-- | Main poll loop. Calls `fn` when it is time to poll
+runPoll
+  :: forall m.
+     (MonadUnliftIO m)
+  => Th.Gate
+  -> m Bool
+  -> m ()
+runPoll gate fn = forever $ do
+  Th.waitForOpenGateAndClose gate
+  runFetch
+
+  where
+    runFetch = do
+      putText "polling"
+      gotItem <- fn
+
+      -- If there is an item then there is probably another, try again immediately
+      when gotItem runFetch
+
+
+-- | Triggers the poll periodically
+runTriggerPoll
+  :: forall m.
+     (MonadUnliftIO m)
+  => Int
+  -> Th.Gate
+  -> m ()
+runTriggerPoll seconds gate = forever $ do
+  UC.threadDelay $ 1000000 * seconds
+  Th.openGate gate
---
+++ src/Threading.hs
@@ -0,0 +1,50 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Threading
+    ( Gate(..)
+    , newOpenGate
+    , newClosedGate
+    , closeGate
+    , openGate
+    , waitForOpenGateAndClose
+    , waitForOpenGate
+    , isOpenGate
+    ) where
+
+import           Verset
+import           UnliftIO.STM (atomically)
+import qualified UnliftIO.STM as US
+import           UnliftIO (MonadUnliftIO)
+
+
+---------------------------------------------------------------------------------------------------------------------
+-- Gate
+---------------------------------------------------------------------------------------------------------------------
+-- | Simple Gate synchronisation type
+-- A gate can be opened or closed
+-- If open any number of threads can continue
+-- If closed nothing can continue
+newtype Gate = Gate (US.TMVar ())
+
+newOpenGate :: (MonadUnliftIO m) => m Gate
+newOpenGate = Gate <$> US.newTMVarIO ()
+
+newClosedGate :: (MonadUnliftIO m) => m Gate
+newClosedGate = Gate <$> US.newEmptyTMVarIO
+
+closeGate :: (MonadUnliftIO m) => Gate -> m ()
+closeGate (Gate g) = void . atomically $ US.tryTakeTMVar g
+
+openGate :: (MonadUnliftIO m) => Gate -> m ()
+openGate (Gate g) = void . atomically $ US.tryPutTMVar g ()
+
+waitForOpenGateAndClose :: (MonadUnliftIO m) => Gate -> m ()
+waitForOpenGateAndClose (Gate g) = void . atomically $ US.takeTMVar g
+
+waitForOpenGate :: (MonadUnliftIO m) => Gate -> m ()
+waitForOpenGate (Gate g) = void . atomically $ US.readTMVar g
+
+isOpenGate :: (MonadUnliftIO m) => Gate -> m Bool
+isOpenGate (Gate g) = atomically $ not <$> US.isEmptyTMVar g
+
+---------------------------------------------------------------------------------------------------------------------
