c8390a2c3f46c39d171502d1f948c457f910653c
Andre Van Der Merwe <andre@andrevdm.com>
2021-12-22 10:32:43 +0200

    ignore if already queued

--- sql/db/migrations/20211221072601_fn_unblocked.sql
+++ sql/db/migrations/20211221072601_fn_unblocked.sql
@@ -3,7 +3,8 @@ CREATE OR REPLACE FUNCTION fn_bq_queue_all_unblocked(_sys_id uuid) RETURNS VOID 
 BEGIN
   insert into bq_queue
     (wiid, created_at)
-    (select v.wiid, now() from vw_bq_unblocked_unqueued v where v.system_id = _sys_id and (v.ignore_until is null or v.ignore_until < now()));
+    (select v.wiid, now() from vw_bq_unblocked_unqueued v where v.system_id = _sys_id and (v.ignore_until is null or v.ignore_until < now()))
+    on conflict do nothing;
 END
 $$ LANGUAGE PLPGSQL;
 
--- sql/db/schema.sql
+++ sql/db/schema.sql
@@ -149,7 +149,8 @@ CREATE FUNCTION public.fn_bq_queue_all_unblocked(_sys_id uuid) RETURNS void
 BEGIN
   insert into bq_queue
     (wiid, created_at)
-    (select v.wiid, now() from vw_bq_unblocked_unqueued v where v.system_id = _sys_id and (v.ignore_until is null or v.ignore_until < now()));
+    (select v.wiid, now() from vw_bq_unblocked_unqueued v where v.system_id = _sys_id and (v.ignore_until is null or v.ignore_until < now()))
+    on conflict do nothing;
 END
 $$;
 
